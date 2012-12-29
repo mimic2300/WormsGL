e30ec1f68fbd9d3381356e6c2b11572ea7afe869
amertak <kadlec.david@gmail.com>
2012-12-29 18:01:19 +0100

    GameTime.cs

--- Glib/GameTime.cs
+++ Glib/GameTime.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace Glib
 {
+    /// <summary>
+    /// Herní doba.
+    /// </summary>
     public class GameTime
     {
+        private Stopwatch watch;
+
+        public GameTime()
+        {
+            watch = new Stopwatch();
+        }
     }
 }
