67f2472d9048782dcdeaf9b3474575ba3b6825c5
amertak <kadlec.david@gmail.com>
2012-12-29 11:54:41 +0100

    Základ myši.

---
+++ Glib/Input/MouseInput.cs
@@ -0,0 +1,73 @@
+﻿using OpenTK.Input;
+using System;
+using System.Drawing;
+
+namespace Glib.Input
+{
+    /// <summary>
+    /// Abstrakce nad myší.
+    /// </summary>
+    public class MouseInput
+    {
+        GlibWindow window;
+        MouseState state;
+
+        /// <summary>
+        /// Vytvoří nový objekt typu MouseInput.
+        /// </summary>
+        /// <param name="window"></param>
+        public MouseInput(GlibWindow window)
+        {
+            this.window = window;
+
+            window.UpdateFrame += UpdateFrame;
+        }
+
+        private void UpdateFrame(object sender, OpenTK.FrameEventArgs e)
+        {
+            // Aktualizuje state
+            state = Mouse.GetState();
+        }
+
+        /// <summary>
+        /// Vrací nebo nastaví pozici myši.
+        /// </summary>
+        public Point Position
+        {
+            get 
+            {
+                return new Point(state.X, state.Y);
+            }
+            set
+            {
+                Mouse.SetPosition(value.X, value.Y);
+            }
+        }
+
+        /// <summary>
+        /// Vrací, jestli button stisknutý.
+        /// </summary>
+        /// <param name="button">Button</param>
+        public bool IsButtonDown(MouseButton button)
+        {
+            return state.IsButtonDown(button);
+        }
+
+        /// <summary>
+        /// Vrací, jestli je button uvolněný.
+        /// </summary>
+        /// <param name="button">Button</param>
+        public bool IsButtonUp(MouseButton button)
+        {
+            return state.IsButtonUp(button);
+        }
+
+        /// <summary>
+        /// Překryje metodu ToString().
+        /// </summary>
+        public override string ToString()
+        {
+            return String.Format("Mouse position: [{0}, {1}]", Position.X, Position.Y);
+        }
+    }
+}
