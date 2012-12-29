0eabb6c062a68a10d1be14ef45f294fed8220118
mimic <mimic2300@gmail.com>
2012-12-29 11:22:53 +0100

    Hlavní herní okno

--- Glib/GlibWindow.cs
+++ Glib/GlibWindow.cs
@@ -1,6 +1,118 @@
-﻿namespace Glib
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace Glib
 {
-    public class GlibWindow
+    /// <summary>
+    /// Herní okno.
+    /// </summary>
+    public class GlibWindow : GameWindow
     {
+        /// <summary>
+        /// Konstruktor.
+        /// </summary>
+        /// <param name="title">Titulek okna.</param>
+        /// <param name="width">Šířka okna.</param>
+        /// <param name="height">Výška okna.</param>
+        /// <param name="mode">Zobrazovací mód.</param>
+        public GlibWindow(string title, int width, int height, GraphicsMode mode)
+            : base(width, height, mode, title)
+        {
+        }
+
+        /// <summary>
+        /// Pozice okna na ose X.
+        /// </summary>
+        public int X
+        {
+            get { return ClientRectangle.X; }
+        }
+
+        /// <summary>
+        /// Pozice okna na ose Y.
+        /// </summary>
+        public int Y
+        {
+            get { return ClientRectangle.Y; }
+        }
+
+        /// <summary>
+        /// Šířka okna.
+        /// </summary>
+        public int Width
+        {
+            get { return ClientRectangle.Width; }
+        }
+
+        /// <summary>
+        /// Výška okna.
+        /// </summary>
+        public int Height
+        {
+            get { return ClientRectangle.Height; }
+        }
+
+        /// <summary>
+        /// Načtení okna.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // barva pozadí - hodně tmavě šedá
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1f);
+        }
+
+        /// <summary>
+        /// Změna rozlišení okna.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(X, Y, Width, Height);
+        }
+
+        /// <summary>
+        /// Vykreslování do okna.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            Initialize();
+            Update(e);
+            Render(e);
+
+            SwapBuffers(); // prohodí backBuffer a frontBuffer
+        }
+
+        /// <summary>
+        /// Initializace openGL.
+        /// </summary>
+        protected virtual void Initialize()
+        {
+            // vyčistí barevný buffer
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+        }
+
+        /// <summary>
+        /// Aktualizace okna.
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void Update(FrameEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Vykreslení do okna.
+        /// </summary>
+        /// <param name="e"></param>
+        protected abstract void Render(FrameEventArgs e);
     }
 }
